e8482a068d72c37e32c8e157caa8bdacb15a6299
Ondrej Mirtes <ondrej@mirtes.cz>
2019-01-08 10:44:13 +0100

    Test for AssignOp like +=

--- tests/src/DisallowFloatAssignedToVariableRuleTest.php
+++ tests/src/DisallowFloatAssignedToVariableRuleTest.php
@@ -33,6 +33,10 @@ class DisallowFloatAssignedToVariableRuleTest extends RuleTestCase
 				'Cannot assign float to $this->bar[\'test\'] - floats are not allowed.',
 				22,
 			],
+			[
+				'Cannot assign float to $test - floats are not allowed.',
+				31,
+			],
 		]);
 	}
 
